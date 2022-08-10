a67fd8c91044617d40b87d0c813d97ca29f6f913
ingmar <ingmar.mueller@kfw.de>
2022-08-10 15:43:14 +0200

    using local fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,12 +42,12 @@ export default function Home() {
           type="text/css"
         />
         <link
-          href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800"
+          href="../css/google-fonts/css/openSans.css"
           rel="stylesheet"
           type="text/css"
         />
         <link
-          href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic"
+          href="../css/google-fonts/css/merriweather.css"
           rel="stylesheet"
           type="text/css"
         />
